639b7cab3a53a5ede49fd54ab2cfdb0c5d3c736e
Om Prakash Pati <147725015+Syl3r27@users.noreply.github.com>
2025-08-19 13:33:09 +0530

    Remove duplicate HTML structure from App component

--- app/root.tsx
+++ app/root.tsx
@@ -51,19 +51,7 @@ export default function App() {
   },[init])
 
   return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
+    <Outlet />
   );
 }
 
